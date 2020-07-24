78eade44fdb20843b977bffa64788c126b70d0de
Raghav Sharma <get2raghavsharma@gmail.com>
2020-07-24 15:32:16 +0530

    broken our homepage into smaller components and finished styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.styles.scss';
+import MenuItem from '../../components/menu-item/menu-item.component';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <Directory />
+        </div>
+    </div>
+)
+
+export default HomePage 
